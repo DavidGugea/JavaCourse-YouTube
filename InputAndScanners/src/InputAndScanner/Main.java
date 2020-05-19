918a0bdcc271665c4a8255a80267f85d3a1f25f9
David <davidgugea@yahoo.com>
2020-05-19 16:53:57 +0200

    Input and Scanners

---
+++ InputAndScanners/src/InputAndScanner/Main.java
@@ -0,0 +1,13 @@
+package InputAndScanner;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String scanned = sc.next();
+		int x = Integer.parseInt(scanned);
+		
+		System.out.println(scanned);
+	}
+}
