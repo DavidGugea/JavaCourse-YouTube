41160634526b2d324eda8ddd0211da6ef1f37ca7
David <davidgugea@yahoo.com>
2020-05-19 17:26:37 +0200

    If/Else/Else If - statements

---
+++ IfElseElseIf/src/IfElseElseIf/Main.java
@@ -0,0 +1,24 @@
+package IfElseElseIf;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
+		if(s.contentEquals("tim")) {
+			System.out.println("You typed tim.");
+		}
+		else if(s.contentEquals("hello")) {
+			System.out.println("Hi!");
+		}
+		else if(s.contentEquals("hello")) {
+			System.out.println("hello!");
+		}
+		else {
+			System.out.println("asdiasijdaosjd");
+		}
+		
+	}
+}
