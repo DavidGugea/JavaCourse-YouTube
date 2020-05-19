b36f2b933f516aed1f5ab16dd882564003f7dcd8
David <davidgugea@yahoo.com>
2020-05-19 17:51:00 +0200

    Arrays

---
+++ Arrays/src/Arrays/Main.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] newArr = new String[5];
+		newArr[0] = "hello";
+		newArr[1] = "hi";
+		newArr[2] = "tim";
+		newArr[3] = "bill";
+		newArr[4] = "joe";
+		
+		int[] nums = {2, 3, 54, 6, 6};
+		double[] nums2 = {2.0, 3.0};
+		
+		double x = nums2[1];
+		System.out.println(x);
+	}
+
+}
