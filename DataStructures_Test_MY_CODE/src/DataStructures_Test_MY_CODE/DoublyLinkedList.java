eb52c67d64ea538ab5b9724400a5fd241de100e9
David <davidgugea@yahoo.com>
2020-05-20 19:31:11 +0200

    Data Structures - My Code - Finished all the Doubly Linked List INSERTION / DELETION methods

--- DataStructures_Test_MY_CODE/src/DataStructures_Test_MY_CODE/DoublyLinkedList.java
+++ DataStructures_Test_MY_CODE/src/DataStructures_Test_MY_CODE/DoublyLinkedList.java
@@ -117,5 +117,133 @@ public class DoublyLinkedList{
 		}
 	}
 	
+	public void insertAfterNode(DoublyLinkedNode searchNode, int data) {
+		DoublyLinkedNode insert_node = new DoublyLinkedNode(data);
+		insert_node.next = searchNode.next;
+		insert_node.prev = searchNode;
+		
+		if(searchNode.next != null) {
+			searchNode.next.prev = insert_node;
+		}
+		
+		searchNode.next = insert_node;
+	}
+	
+	public void insertAfterNodeWithData(int searchData, int data) {
+		// Get the node with the given data
+		DoublyLinkedNode current = this.head;
+		
+		while(current != null) {
+			if(current.data == data) {
+				break;
+			}
+			current = current.next;
+		}
+		
+		DoublyLinkedNode insert_node = new DoublyLinkedNode(data);
+		insert_node.prev = current;
+		insert_node.next = current.next;
+		
+		if(current.next != null) {
+			current.next.prev = insert_node;
+		}
+		
+		current.next = insert_node;
+	}
+	
+	public void insertAtIndex(int searchIndex, int data) {
+		// Get the node at the given index
+		DoublyLinkedNode current = this.head;
+		int indexTrack = 0;
+		
+		while(indexTrack < searchIndex) {
+			current = current.next;
+			indexTrack++;
+		}
+		
+		DoublyLinkedNode insert_node = new DoublyLinkedNode(data);
+		insert_node.prev = current;
+		insert_node.next = current.next;
+		
+		if(current.next != null) {
+			current.next.prev = current;
+		}
+		
+		current.next = insert_node;
+		
+		if(searchIndex == 0) {
+			// Update the head node;
+			this.head = insert_node;
+		}
+	}
+	
+	public void deleteNode(DoublyLinkedNode node) {
+		// Check if the given node is the head node
+		if(this.head == node) {
+			if(this.head.next != null) {
+				this.head.next.prev = null;
+			}
+			this.head = this.head.next;
+		}else {
+			// Change the pointers
+			DoublyLinkedNode PREV_NODE = node.prev;
+			node.prev.next = node.next;
+			
+			if(node.next != null) {
+				node.next.prev = PREV_NODE;
+			}
+		}
+	}
+	
+	public void deleteAtIndex(int index) {
+		// Check if the given index is 0, so if the node that has to be deleted is the head node
+		if(index == 0) {
+			if(this.head.next != null) {
+				this.head.next.prev = null;
+			}
+			this.head = this.head.next;
+		}else {
+			// Get the node at the given index and delete it
+			DoublyLinkedNode current = this.head;
+			int indexTrack = 0;
+			
+			while(indexTrack < index) {
+				current = current.next;
+				indexTrack++;
+			}
+			
+			current.prev.next = current.next;
+			
+			if(current.next.prev != null) {
+				current.next.prev = current.prev;
+			}
+		}
+	}
+	
+	public void deleteNodeWithData(int data) {
+		// Check if the given data is the same as the data of the head node, so if the node that must be deleted is the head node
+		if(this.head.data == data) {
+			if(this.head.next != null) {
+				this.head.next.prev = null;
+			}
+			this.head = this.head.next;
+		}else {
+			// Get the first node with the given data and delete it
+			DoublyLinkedNode current = this.head;
+			while(current != null) {
+				if(current.data == data) {
+					break;
+				}
+				current = current.next;
+			}
+			
+			current.prev.next = current.next;
+			
+			if(current.next != null) {
+				current.next.prev = current.prev;
+			}
+		}
+	}
+	
 	/* INSERTION / DELETION */
 }
